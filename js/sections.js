a32348274647f4de8118612deaf0a0cdcffb4803
HATSUMATSU <mail@hatsumatsu.de>
2016-11-01 10:38:48 +0100

    added sections.js

---
+++ js/sections.js
@@ -0,0 +1,132 @@
+/**
+ * Sections
+ */
+var Sections = ( function() {
+    var settings = {
+        current: {
+            id: undefined
+        },
+        _current: {
+            id: undefined
+        },
+        offset: 0,
+        snap: false
+    }
+
+    var selector = {
+        section: '[data-section]',
+    }
+
+    var cache = [];
+
+    var init = function() {
+        Debug.log( 'Sections.init();' );
+
+        onResize();
+        onScroll();
+
+        bindEventhandlers();
+    }
+
+    var bindEventhandlers = function() {
+        $( document )
+            .on( 'viewport/scroll', function() {
+                onScroll();
+            } )
+            .on( 'viewport/resize/finish', function() {
+                onResize();
+            } )
+            .on( 'sections/change', function( event, data ) {
+                onChange( data.section );
+            } )
+            .on( 'sections/leave', function( event, data ) {
+                onLeave( data.section );
+            } )
+            .on( 'sections/enter', function( event, data ) {
+                onEnter( data.section );
+            } );
+    }
+
+    var onScroll = function() {
+        var scrollTop = Viewport.get( 'scrollTop' );
+
+        for( var i = 0; i < cache.length; i++ ) {
+            if( scrollTop >= cache[i].top - settings.offset && scrollTop < cache[i].bottom - settings.offset ) {
+                settings._current = settings.current;
+
+                if( cache[i].id !== settings._current.id || !settings.current.id ) {
+                    settings.current = cache[i];
+
+                    $( document ).trigger( 'sections/change', [{ section: settings.current }] );
+                    $( document ).trigger( 'sections/enter', [{ section: settings.current }] );
+                    $( document ).trigger( 'sections/leave', [{ section: settings._current }] );
+                }
+            }
+        }
+    }
+
+    var onResize = function() {
+        settings.offset = ( Viewport.get( 'height' ) / 2 );
+
+        buildCache();
+    }
+
+    var onChange = function( section ) {
+        Debug.log( 'Sections.onChange()', section );
+
+        if( !section.id ) {
+            return false;
+        }
+    }
+
+    var onEnter = function( section ) {
+        Debug.log( 'Sections.onEnter()', section );
+
+        if( !section.id ) {
+            return false;
+        }
+
+        section.section
+            .addClass( 'current' );
+    }
+
+    var onLeave = function( section ) {
+        Debug.log( 'Sections.onLeave()', section );
+
+        if( !section.id ) {
+            return false;
+        }
+
+        section.section
+            .removeClass( 'current' );
+    }
+
+    var buildCache = function() {
+        Debug.log( 'Sections.cache();' );
+
+        cache = [];
+
+        $( selector.section ).each( function() {
+            var section = $( this );
+            var top = section.offset().top;
+            var bottom = top + section.outerHeight();
+
+            cache.push( {
+                id: section.attr( 'data-section' ),
+                section: section,
+                top: top,
+                bottom: bottom
+            } );
+        } );
+
+        Debug.log( cache );
+    }
+
+    return {
+        init: function() { init(); }
+    }
+} )();
+
+$( document ).ready( function() {
+    Sections.init();
+} );
